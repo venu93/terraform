f8c4d42603169cb25e2fe4c0f2e6c1907a0c445a
venugopal <venugopal.batthina@gmail.com>
2021-02-21 12:17:41 +0530

    first commit

---
+++ terraform create/createinstance.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    access_key = "AKIA6L7HCWTVJKR2I2VX"
+    secret_key = "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"
+    region     = "us-east-2"
+}
+
+resource "aws_instance" "MyfirstInstance" {
+    ami             = "ami-0b6fd73535e4b992b"
+    instance_type   = "t2.micro"
+}
