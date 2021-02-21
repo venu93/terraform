7a3fa64db88445159d352d20ff5cc2bffb9aee94
venugopal <venugopal.batthina@gmail.com>
2021-02-21 13:00:21 +0530

    fd

--- terraform create/createinstance.tf
+++ terraform create/createinstance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
     access_key = "AKIA6L7HCWTVG3ZFPPYU"
     secret_key = "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"
-    region     = "us-east-1"
+    region     = "ap-south-1"
 }
 
 resource "aws_instance" "MyfirstInstance" {
-    ami             = "ami-00801a692d6c8a45d"
+    ami             = "ami-01e5bce62744b5c68"
     instance_type   = "t2.micro"
 }
